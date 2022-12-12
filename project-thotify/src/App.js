d3f0f4abf25fd62c8d3d93e22e85e5174b31723a
Jacob Wood <jwood7@stevens.edu>
2022-12-12 11:42:09 -0500

    Made Routes and Initial Matches Layout

--- project-thotify/src/App.js
+++ project-thotify/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {NavLink, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from '.components/Home';
+import Profile from '.components/Profile';
+import Matches from '.components/Matches';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+          <header className='App-header'>
+            <h1 className='App-title'>
+              Thotify
+            </h1>
+            <nav>
+              <NavLink className='navlink' to='/'>
+                Home
+              </NavLink>
+              |
+              <NavLink className='navlink' to='/profile/0'> 
+               {/*Need to change link above to logged in user's profile, or redirect to login*/}
+                My Profile
+              </NavLink>
+              |
+              <NavLink className='navlink' to='/my-matches'>
+                My Matches
+              </NavLink>
+            </nav>
+          </header>
+          <div className='App-body'>
+            <Routes>
+              <Route exact path='/' element={<Home/>} />
+              <Route path='/profile/:id' element={<Profile/>} />
+              <Route path='/my-matches/' element={<Matches/>} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
