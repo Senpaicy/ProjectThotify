78aa5f562e76a12a58e305e4442f719e821984dd
Jacob Wood <jwood7@stevens.edu>
2022-12-15 17:56:52 -0500

    Made message connect automatically to socket on opening message page

--- project-thotify/src/components/Message.js
+++ project-thotify/src/components/Message.js
@@ -8,6 +8,7 @@ function Message() {
   // const location = useLocation();
   // const [state, setState] = useState(location.state);
   // temporary state for testing purposes
+  // currently has zero protection against unauthorized users joining the chat
   const [state, setState] = useState({message: '', name: '', room: 'A', recipient: 'Frank', history:[{name: "Bob", message: "Hi"},{name: "Frank", message: "Hello"}]});
   const [chat, setChat] = useState([]);
 
@@ -16,7 +17,7 @@ function Message() {
   useEffect(() => {
     socketRef.current = io('/');
     // setState({name: document.getElementById('username_input').value, room: document.getElementById('room_input').value});
-    // socketRef.current.emit('create_room', "Bob", "A");
+    socketRef.current.emit('create_room', "Bob", "A");
     // console.log("Room Created");
     return () => {
       socketRef.current.disconnect();
@@ -27,33 +28,13 @@ function Message() {
     socketRef.current.on('message', ({name, message}) => {
       setChat([...chat, {name, message}]);
     });
-    console.log("Message recieved");
-    console.log(chat);
-    // don't need messages for user joining and leaving the chat 
-    // socketRef.current.on('user_join', function (data) {
-    //   setChat([
-    //     ...chat,
-    //     {name: 'ChatBot', message: `${data} has joined the chat`},
-    //   ]);
-    // });
-    // socketRef.current.on('user_leave', function (data) {
-    //   setChat([
-    //     ...chat,
-    //     {name: 'ChatBot', message: `${data} has left the chat`},
-    //   ]);
-    // });
   }, [chat]);
-
-  const createroom = (name, room) => {
-    socketRef.current.emit('create_room', name, room);
-  };
 
   const onMessageSubmit = (e) => {
     console.log("Message send called");
     let msgEle = document.getElementById('message');
     console.log([msgEle.name], msgEle.value);
     setState({...state, [msgEle.name]: msgEle.value});
-    console.log(state);
     socketRef.current.emit('message', {
       name: state.name,
       message: msgEle.value,
@@ -84,8 +65,6 @@ function Message() {
   };
 
   const renderChat = () => {
-    console.log("render chat called");
-    console.log(chat);
     return chat.map(({name, message}, index) => (
       <div key={index}>
         <h3>
@@ -123,37 +102,6 @@ function Message() {
         </div>
       )}
 
-
-      {!state.name && (
-        <form
-          className='form'
-          onSubmit={(e) => {
-            console.log(document.getElementById('username_input').value);
-            e.preventDefault();
-            setState({name: document.getElementById('username_input').value, room: document.getElementById('room_input').value, recipient:"Frank", history:[{name: "Bob", message: "Hi"},{name: "Frank", message: "Hello"}]});
-            // userjoin(document.getElementById('username_input').value);
-            createroom(document.getElementById('username_input').value, document.getElementById('room_input').value);
-            // userName.value = '';
-          }}
-        >
-          <div className='form-group'>
-            <label>
-              User Name:
-              <br />
-              <input id='username_input' />
-              <br />
-              Room ID:
-              <br />
-              <input id='room_input' />
-            </label>
-          </div>
-          <br />
-
-          <br />
-          <br />
-          <button type='submit'> Click to join</button>
-        </form>
-      )}
     </div>
   );
 };
