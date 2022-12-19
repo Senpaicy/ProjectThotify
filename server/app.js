9b0e2137b9dc59b021c956b22bc2d1997610f806
FarhanShaik <farshaik@gmail.com>
2022-12-19 11:22:10 -0800

    setup up production buil #1 for testing

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
 var cors = require("cors");
 var cookieParser = require("cookie-parser");
+const path = require("path");
+
 const app = express();
 const configRoutes = require("./routes");
+
 
 app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("../project-thotify/build"));
+
+app.get("*", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, '../project-thotify/build/index.html'));
+});
 
 configRoutes(app);
+
+
+
 
 let port = process.env.PORT || 8888;
 app.listen(port, () => {
   console.log(`Running locally on: http://localhost:${port}`);
 });
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,6 @@ const userRoutes = require("./users");
 const constructorMethod = (app) => {
   app.use("/spotify", spotifyRoutes);
   app.use("/users", userRoutes);
-
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Page not found" });
-  });
 };
 
 module.exports = constructorMethod;
