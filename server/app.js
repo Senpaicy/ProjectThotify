3538f164c4ad0333feb18b19f9560d8ed44f481d
FarhanShaik <farshaik@gmail.com>
2022-12-20 14:29:32 -0800

    modified server/app.js for prod deployment

--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
 const express = require("express");
 var cors = require("cors");
 var cookieParser = require("cookie-parser");
+const path = require("path");
 const fileUpload = require('express-fileupload');
+
+
 
 const app = express();
 const configRoutes = require("./routes");
-
-app.use(cors());
 
 // enable files upload
 app.use(fileUpload({
   createParentPath: true
 }));
 
+
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("../project-thotify/build"));
+
+app.get("*", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, '../project-thotify/build/index.html'));
+});
 
 configRoutes(app);
+
+
+
 
 let port = process.env.PORT || 8888;
 app.listen(port, () => {
   console.log(`Running locally on: http://localhost:${port}`);
 });
+
